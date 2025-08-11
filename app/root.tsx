3a457c03cd24c0deb93cda0665e2016a26034205
jdc2000 <25034907+jdc2000@users.noreply.github.com>
2025-08-10 20:05:50 -0500

    Add theme

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,8 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import "./app.css";
+import { ThemeProvider } from "~/components/theme-provider";
+import "~/app.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -42,7 +43,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="rmxui-theme">
+      <Outlet />
+    </ThemeProvider>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,9 +1,25 @@
 import { Outlet } from "react-router";
+import { useTheme } from "~/components/theme-provider";
 
 export default function Layout() {
+  const { theme, setTheme } = useTheme();
+  function toggleTheme() {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  }
   return (
-    <main>
-      <Outlet />
+    <main className="flex gap-[24px] min-h-screen">
+      <div className="flex-col">
+        <button className="cursor-pointer" onClick={toggleTheme}>
+          {theme === "dark" ? "light" : "dark"}
+        </button>
+      </div>
+      <div>
+        <Outlet />
+      </div>
     </main>
   );
 }
